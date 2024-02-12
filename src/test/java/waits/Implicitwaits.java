2be23a77203253a52bfae50f92b14ba588f5ceea
ndamroland <infos.ndam.roland@gmail.com>
2024-02-12 01:48:59 +0100

    My First test

--- src/test/java/waits/Implicitwaits.java
+++ src/test/java/waits/Implicitwaits.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class Implicit_waits extends Hooks {
+public class Implicitwaits extends Hooks {
 
     @Test
     public void loginPage(){
