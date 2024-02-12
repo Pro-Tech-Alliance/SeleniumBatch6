79e51d33447c1e1fbc35df91f1d310e2c75ade64
ndamroland <infos.ndam.roland@gmail.com>
2024-02-12 02:18:59 +0100

    Implicit_Waits Completed

---
+++ src/test/java/waits/Implicit_Waits.java
@@ -0,0 +1,29 @@
+package waits;
+
+import base.Hooks;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class Implicit_Waits extends Hooks {
+
+
+
+
+    String baseUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+
+    @Test
+    public void implicit_Wait (){
+
+        driver.get(baseUrl);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+
+
+    }
+
+
+
+
+}
