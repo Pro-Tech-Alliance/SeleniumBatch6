5a57fa981cb89eb1fa7e30c669756bb822ab92ec
Prof Milo <emileesapa@gmail.com>
2024-02-13 04:42:57 -0600

    refator

--- src/test/java/New_Tab/New_Tab.java
+++ src/test/java/New_Tab/New_Tab.java
@@ -7,8 +7,7 @@ import org.testng.annotations.Test;
 public class New_Tab extends Hooks {
     @Test
     public void newTab() throws InterruptedException {
-///sdgergthttseh5h5h5
-        //rthh
+
         driver.switchTo().newWindow(WindowType.TAB);//switch to the new tab
         driver.navigate().to("https://www.google.com/");//after switching to the new tab...navigate to this url
 
